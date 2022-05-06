35df34e78f7621fa948a12f6f22001a8fa7342e7
courtcoder <courtney1187@icloud.com>
2022-05-06 15:50:11 -0400

    added password length user input

--- script.js
+++ script.js
@@ -5,26 +5,21 @@
 var pwLength = '';
 var password = '';
 var chars = '';
-var charsG = '';
+var genChars = '';
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var numberChars = '0123456789';
 var specialChars = '!@#$%^&*()';
 
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// USER INPUT- PASSWORD LENGTH
+function pwPrompt () {
+    pwLength = prompt("How long would you like your Password?");
+    if (pwLength < 8 || pwLength > 128) {
+        alert("Password must be between 8 and 128 characters long.");
+    }  if (isNaN(pwLength)) {
+            alert("Password must be a number");  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
